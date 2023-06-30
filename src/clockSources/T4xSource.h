ea6f850ec2e8c0eedd7ae32714a09f75310651e3
luni64 <lutz@niggl-schlagbauer.de>
2023-06-30 10:25:10 +0200

    replaced the rather rude noInterupt/interrupt sequence by  LDREX/STREX protection pattern

--- src/clockSources/T4xSource.h
+++ src/clockSources/T4xSource.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Arduino.h"
+#include "arm_math.h" // __LDREXW
 #include <chrono>
 
 class T4X_Source
@@ -20,22 +21,28 @@ class T4X_Source
 
     static tickType getTicks()
     {
-        noInterrupts();
-        uint32_t curLow = ARM_DWT_CYCCNT;
-        if (curLow < oldLow) // roll over
-        {
-            curHigh++;
-        }
-        oldLow          = curLow;
-        uint64_t curVal = ((uint64_t)curHigh << 32) | curLow;
-        interrupts();
+        uint32_t monitor; // used to monitor if an interrupt occured in the critical section
+        uint32_t curVal;
+
+        do { // interrupts between __LDREXW and _STREXW make the STREXW call failing, in this case we recalculate (see https://community.arm.com/support-forums/f/architectures-and-processors-forum/10361/ldrex-strex-on-the-m3-m4-m7/33045#33045)
+            __LDREXW(&monitor);
+
+            uint32_t curLow = ARM_DWT_CYCCNT;
+            if (curLow < oldLow) // roll over
+            {
+                curHigh++;
+            }
+            oldLow = curLow;
+            curVal = ((uint64_t)curHigh << 32) | curLow;
+
+        } while (__STREXW(1, &monitor)); // returns != 0 if an interrupt occured in the block
 
         return curVal;
     }
 
     static tickType getRTC()
     {
-        return (tickType) rtc_get();
+        return (tickType)rtc_get();
     }
 
  protected:
